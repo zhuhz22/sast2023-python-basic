6e447acd7d3b1687836539a29d723eafdbeaeb93
朱泓舟 <zhuhz22@mails.tsinghua.edu.cn>
2023-07-20 17:08:22 +0800

    GUI初步

---
+++ GUI.py
@@ -0,0 +1,53 @@
+import argparse
+import json
+import streamlit as st
+
+def read_articles(filename):
+    data=json.load(filename)
+    return data
+
+def replace(article, keys):
+    for i in range(len(keys)):
+        tmp=str(i+1)
+        article=article.replace("{{"+tmp+"}}",keys[i])
+    return article
+
+
+if __name__ == "__main__":
+
+    st.title("Filling Word Game")
+    file=st.file_uploader("选择题库",type='json')
+    
+    if file is not None:
+        data = read_articles(file)
+        articles = data["articles"] 
+        
+        options=[]
+        index={}
+        for num in range(len(articles)):
+            options.append(articles[num]['title'])
+            index[articles[num]['title']]=num
+        option=st.selectbox("Title of the article you choose is: ",options)
+    
+        title=articles[index[option]]['title']
+        article=articles[index[option]]['article']
+        hints=articles[index[option]]['hints']
+        
+        st.write("bacis information of the article:\n")
+        st.write("title:\t ",title)
+        st.write("article:\t ",article)
+        
+        keys = []
+        _=0
+        flag=False
+        for hint in hints:
+            _+=1
+            keys.append(st.text_input(f"请输入{hint}:",key=str(_)))
+        if len(keys[-1])>0:
+            flag=True
+            
+        if flag==True:    
+            result=replace(article, keys)
+            st.write("The result is:\n",result)
+
+
--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import argparse
 import json
 
-def parser_data():
-    """
-    从命令行读取用户参数
-    做出如下约定：
-    1. -f 为必选参数，表示输入题库文件
-    2. -s 为必选参数，表示选用题库中文章序号（从零开始）
 
-    :return: 参数
-    """
+def parser_data():
     parser = argparse.ArgumentParser(
         prog="Word filling game",
         description="A simple game",
@@ -25,13 +18,6 @@ def parser_data():
 
 
 def read_articles(filename):
-    """
-    读取题库文件
-
-    :param filename: 题库文件名
-
-    :return: 一个字典，题库内容
-    """
     with open(filename, 'r', encoding="utf-8") as f:
         data=json.load(f)
     return data
@@ -39,36 +25,17 @@ def read_articles(filename):
 
 
 def get_inputs(hints):
-    """
-    获取用户输入
-
-    :param hints: 提示信息
-
-    :return: 用户输入的单词
-    """
-
     keys = []
     for hint in hints:
         print(f"请输入{hint}:")
         keys.append(input())
-
     return keys
 
 
 def replace(article, keys):
-    """
-    替换文章内容
-
-    :param article: 文章内容
-    :param keys: 用户输入的单词
-
-    :return: 替换后的文章内容
-
-    """
     for i in range(len(keys)):
         tmp=str(i+1)
-        new_article=article.replace("{{"+tmp+"}}",keys[i])
-        article=new_article
+        article=article.replace("{{"+tmp+"}}",keys[i])
         # 将 article 中的 {{i}} 替换为 keys[i]
     return article
 
@@ -76,9 +43,9 @@ def replace(article, keys):
 if __name__ == "__main__":
     args = parser_data()
     data = read_articles(args.file)
-    articles = data["articles"]#articles is a list
+    articles = data["articles"]    #articles is a list
 
-    # 根据参数或随机从 articles 中选择一篇文章    elements of list article is dictionaries
+    # 根据参数或随机从 articles 中选择一篇文章    #elements of list article is dictionaries
     title=articles[int(args.sequence)]['title']
     article=articles[int(args.sequence)]['article']
     hints=articles[int(args.sequence)]['hints']
